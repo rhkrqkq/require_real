a5745d24b22ff0c39f051127b7150dc6e0f61a20
rhkrqkq <rhkrqkq1025@gmail.com>
2026-04-09 14:29:03 +0900

    chore: init

---
+++ src/main/java/org/example/require/RequireApplication.java
@@ -0,0 +1,13 @@
+package org.example.require;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/require/RequireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.require;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
