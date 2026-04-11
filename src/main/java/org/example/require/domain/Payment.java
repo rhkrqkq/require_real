8202e7a9d80447e2993b6b9adb24af4f44dd3439
rhkrqkq <rhkrqkq1025@gmail.com>
2026-04-11 17:21:57 +0900

    refactor: 엔티티 객체 생성 위한 빌더 추가

--- src/main/java/org/example/require/domain/Payment.java
+++ src/main/java/org/example/require/domain/Payment.java
@@ -1,11 +1,17 @@
 package org.example.require.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.require.domain.enums.PaymentStatus;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +29,10 @@ public class Payment {
     private PaymentStatus paymentStatus;
 
     private LocalDateTime paidAt;
+
+    @Builder
+    public Payment(Reservation reservation, int amount) {
+        this.reservation = reservation;
+        this.amount = amount;
+    }
 }
--- src/main/java/org/example/require/domain/Reservation.java
+++ src/main/java/org/example/require/domain/Reservation.java
@@ -1,11 +1,14 @@
 package org.example.require.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.example.require.domain.enums.ReservationStatus;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +31,12 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     private ReservationStatus reservationStatus;
 
+    @Builder
+    public Reservation(User user, Seat seat, LocalDateTime startTime, LocalDateTime endTime) {
+        this.user = user;
+        this.seat = seat;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.createdAt = LocalDateTime.now();
+    }
 }
